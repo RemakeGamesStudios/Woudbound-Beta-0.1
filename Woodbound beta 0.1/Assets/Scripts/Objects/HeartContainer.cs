36bc3d109246ed258840e58dfcc4c895fddca0c8
JoAMD <joasus12345@gmail.com>
2019-06-30 09:55:00 +0300

    Player collects only a max of 5 hearts for static as well as dropped hearts; HeartContainer.cs and Heart.cs

--- Woodbound beta 0.1/Assets/Scripts/Objects/HeartContainer.cs
+++ Woodbound beta 0.1/Assets/Scripts/Objects/HeartContainer.cs
@@ -6,13 +6,24 @@ public class HeartContainer : Powerup
 {
     public FloatValue heartContainers;
     public FloatValue playerHealth;
+    [SerializeField]
+    private bool isCollectedHeart = false;
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.CompareTag("Player"))
+        if(!isCollectedHeart && playerHealth.RuntimeValue < 4.5f && other.gameObject.CompareTag("Player"))
         {
+            isCollectedHeart = true;
             heartContainers.RuntimeValue += 1;
             playerHealth.RuntimeValue++;
+            /*
+            playerHealth.RuntimeValue++;
+            if (playerHealth.RuntimeValue > 5)
+            {
+                playerHealth.RuntimeValue = 5;
+            }
+            heartContainers.RuntimeValue = playerHealth.RuntimeValue;
+            */
             powerupSignal.Raise();
             Destroy(this.gameObject);
         }
