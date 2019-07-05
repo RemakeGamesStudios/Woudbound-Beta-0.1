17ace7b88a3b6874726c09b6069dbcdf6a135008
JoAMD <joasus12345@gmail.com>
2019-07-05 18:21:03 +0300

    Adding remaining changes before checkout

--- Woodbound beta 0.1/Assets/Scripts/Game Stuff/Knockback.cs
+++ Woodbound beta 0.1/Assets/Scripts/Game Stuff/Knockback.cs
@@ -21,12 +21,13 @@ public class Knockback : MonoBehaviour {
             {
                 Vector2 difference = hit.transform.position - transform.position;
                 difference = difference.normalized * thrust;
-                hit.AddForce(difference, ForceMode2D.Impulse);
+                hit.AddForce(difference * 2, ForceMode2D.Impulse);
+                Debug.Log("adding force to " + other.name + " force = " + difference * 2);
                 //StartCoroutine(stopKnockBackCo(hit, difference));
                 if (other.gameObject.CompareTag("enemy") && other.isTrigger && 
                     GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().currentState == PlayerState.attack)
                 {
-                    Debug.Log("hittt enemy");
+                    //Debug.Log("hittt enemy");
                     hit.GetComponent<Enemy>().currentState = EnemyState.stagger;
                     other.GetComponent<Enemy>().Knock(hit, knockTime, damage);
                 }
@@ -34,7 +35,7 @@ public class Knockback : MonoBehaviour {
                 {
                     if (other.GetComponent<PlayerMovement>().currentState != PlayerState.stagger)
                     {
-                        Debug.Log("hittt player");
+                        //Debug.Log("hittt player");
                         hit.GetComponent<PlayerMovement>().currentState = PlayerState.stagger;
                         other.GetComponent<PlayerMovement>().Knock(knockTime, damage); 
                     }
