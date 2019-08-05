e3f3fb9abd3e9774abee87a6a1391c1d512f90d4
JoAMD <joasus12345@gmail.com>
2019-08-05 13:55:12 +0530

    Progress in Farming. Added dialogue box and a simple plough patch function with E. Todo : handle all other farming functions/states efficiently

---
+++ Woodbound beta 0.1/Assets/Scripts/Farming/FarmingPatch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class FarmingPatch : Sign
+{
+    public Tilemap tilemap;
+    public TileBase tileBase;
+    public override void Update()
+    {
+        base.Update();
+        if (Input.GetKeyDown(KeyCode.E) && playerInRange)
+        {
+            //play suitable animation for plough patch, sow seeds, tend, or harvest
+            Vector3Int pos = new Vector3Int(-57, 4, 0);
+            for (int i = 0; i < 3; i++)
+            {
+                tilemap.SetTile(pos, tileBase);
+                pos.y++;
+            }
+            //-57, 4 to 6
+        }
+    }
+
+}
