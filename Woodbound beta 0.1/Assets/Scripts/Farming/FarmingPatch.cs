bf726358d571aef828858030f792252f5e9d5a0e
JoAMD <joasus12345@gmail.com>
2019-08-05 23:06:13 +0530

    Done with farming cycles (ploughing, growing, harvesting)

--- Woodbound beta 0.1/Assets/Scripts/Farming/FarmingPatch.cs
+++ Woodbound beta 0.1/Assets/Scripts/Farming/FarmingPatch.cs
@@ -11,21 +11,30 @@ public class FarmingPatch : Sign
     private string statusStr = "plough";
     [SerializeField]
     private int status = 0;
-    private bool isFirstTime = true, isHarvesting = false;
+    [SerializeField]
+    private bool isFirstTime = true;
+    private IEnumerator co;
+
+    private void Start()
+    {
+        co = UpdatePatchStatus();
+    }
 
     public override void Update()
     {
         base.Update();
 
-        if (Input.GetKeyDown(KeyCode.E) && playerInRange && status != 1 && !isHarvesting)
+        if (Input.GetKeyDown(KeyCode.E) && playerInRange && status != 1)
         {
             if (isFirstTime)
             {
-                StartCoroutine(UpdatePatchStatus());
+                Debug.Log("Sowing!");
+                StartCoroutine(co);
                 isFirstTime = false;
             }
             else if(status >= 2)
             {
+                StopCoroutine(co);
                 Harvest();
             }
         }
@@ -34,18 +43,18 @@ public class FarmingPatch : Sign
     private IEnumerator UpdatePatchStatus()
     {
         int i = status;
-        while (i < 4 && !isHarvesting)
+        while (i < 4)
         {
             i = status + 1;
+            if(i > 3)
+            {
+                break;
+            }
             //play suitable animation for plough patch, sow seeds, tend, or harvest
             Vector3Int pos = new Vector3Int(-57, 4, 0);
             SetTiles(pos, i);
             status = i;
-            if (isHarvesting)
-            {
-                yield return null;
-                break;
-            }
+            Debug.Log("growing " + i);
             yield return new WaitForSeconds(3f);
             i++;
         }
@@ -53,13 +62,12 @@ public class FarmingPatch : Sign
 
     private void Harvest()
     {
-        isHarvesting = true;
         Debug.Log("Harvesting!!");
         Vector3Int pos = new Vector3Int(-57, 4, 0);
         SetTiles(pos, 0);
         status = 0;
         isFirstTime = true;
-        isHarvesting = false;
+        co = UpdatePatchStatus();
     }
 
     private void SetTiles(Vector3Int pos, int i)
