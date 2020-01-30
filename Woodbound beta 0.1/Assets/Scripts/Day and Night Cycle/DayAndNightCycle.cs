48874c95f9b0f96660a18656abf2a50bb2022f5d
JoAMD <joasus12345@gmail.com>
2020-01-30 13:54:25 +0530

    Added Day and night cycle

---
+++ Woodbound beta 0.1/Assets/Scripts/Day and Night Cycle/DayAndNightCycle.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayAndNightCycle : MonoBehaviour
+{
+
+    public Light myLight;
+ 
+    
+    float curTime =0;
+    
+    public bool isNight = true;
+    public int weather = 50;
+    public float inten=1;
+    public int maxTime = 120;
+    [SerializeField]
+   
+
+
+     void Start()
+    {
+        myLight.GetComponent<Light>();
+        inten = myLight.intensity;
+        
+
+    }
+    void Update()
+    {
+       
+            
+            
+            if (isNight == true)
+            {
+            for (float i = 0; i < maxTime; i++)
+            {
+                myLight.intensity -= .5f;
+                curTime += Time.deltaTime;
+            }
+            
+                isNight = false;
+                
+            
+            } 
+            else if(isNight == false)
+            {
+               
+            for (float i = maxTime; i >1; i--)
+            {
+                myLight.intensity += .5f;
+                curTime += 1 * Time.deltaTime;
+            }
+              
+                isNight = true;
+                
+            }
+            
+            
+
+    }
+}
---
+++ Woodbound beta 0.1/Assets/Scripts/Day and Night Cycle/Snow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snow : MonoBehaviour
+{
+    float time = 0;
+    float Rng = 11;
+    private ParticleSystem snow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        snow = GetComponent<ParticleSystem>();
+        snow.Stop();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time == 90)
+        {
+            Rng = Random.Range(0, 100);
+            time = 0;
+            if (Rng <= 10)
+            {
+                snow.Play();
+            }
+        }
+    }
+}
